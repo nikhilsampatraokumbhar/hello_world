f980f10892799ccd324225a4623ffefcb601aef9
nikhilsampatraokumbhar <40244395+nikhilsampatraokumbhar@users.noreply.github.com>
2018-06-29 10:25:35 +0530

    Create appendot.java

---
+++ appendot.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class appendot
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        String s1=str+".";
+        System.out.print(s1);
+    }
+}
